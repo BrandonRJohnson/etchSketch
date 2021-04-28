5f358851869309a19f50e332bba38c4520f15b0d
Brandon Johnson <brj.webside@gmail.com>
2021-04-28 15:10:45 -0500

    Attempting to create a dropdown button with some basic options. Will change the dropdown to effect the grid size in a later commit

--- etchSketch.js
+++ etchSketch.js
@@ -12,19 +12,13 @@ function makeRows(rows, cols) {
 
 makeRows(4, 4);
 
+function adjustGrid(userInput) {
+    
+}
+// need to get the grid boxes to change color
 
-// Need to create 64 squares in container div using DOM
-// Wondering here if we can use a while loop to create these 64 squares instead of doing them all individually 
+// need to get the grid boxes to change color based on hover
+document.getElementById("grid-item").addEventListener("click",);
 
-// Those squares need to be formatted into a larger square using CSS
-
-// Set up a hover effect so that the grid divs
-    // Can set up event listeners in order to receive that input
-
-// Make the color of the divs change
-    // Option 1: adding a new class to the div1
-    // Option 2: changing the div's background color using javascript
-
-// Add a button at the top of the screen to clear the current grid
-
+// include userInput box so the user can determine the size of the grid *work on this next I think it would be the easiest to get working*
 

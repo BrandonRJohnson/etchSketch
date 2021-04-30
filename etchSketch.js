07b48e6c27826a6a2195f80b08221ed5d536a9be
Brandon Johnson <brj.webside@gmail.com>
2021-04-30 15:17:11 -0500

    Changes made with Quinn to make project work

--- etchSketch.js
+++ etchSketch.js
@@ -7,23 +7,28 @@ function makeRows(rows, cols) {
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-    cell.setAttribute("id", "sketch");
+    cell.setAttribute("id", "sketch" + `${c}` );
   };
 };
 
 makeRows(4, 4);
 
-let gridItem = document.getElementsByClassName("grid-item");
+// let gridItem = document.getElementsByClassName("grid-item");
+let gridItems = document.getElementsByClassName("grid-item");
 
 function changeColor() {
-  document.getElementById("sketch").style.backgroundColor = "magenta";
-  console.log("1")
+  // document.getElementById(`sketch${i}`).style.backgroundColor = "magenta";
+  gridItems[4].style.backgroundColor = "peachpuff";
+  console.log("1");
 }
-document.getElementById("sketch").addEventListener("mouseenter", changeColor);
-document.getElementById("sketch").addEventListener("mouseover", changeColor); 
 
-function adjustGrid() {
-    return "poop poop";
+// document.getElementsByClassName("grid-item").addEventListener("mouseover", changeColor); 
+
+for (i=0; i < gridItems.length; i++) {
+  // gridItems[i].addEventListener("mouseenter",changeColor);
+  gridItems[i].addEventListener("mouseenter", function(e) {
+    e.target.style.background = "papayawhip";
+  })
 }
 
 // include userInput box so the user can determine the size of the grid *work on this next I think it would be the easiest to get working

a35154f53f301119a83e0568e1c26ca9ad8a3fb0
Brandon Johnson <brj.webside@gmail.com>
2021-05-04 21:33:07 -0500

    Made it so you can reset the game. Also made it so the game generates random colors.

--- etchSketch.js
+++ etchSketch.js
@@ -7,28 +7,41 @@ function makeRows(rows, cols) {
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-    cell.setAttribute("id", "sketch" + `${c}` );
+    cell.setAttribute("id", `${c}` );
   };
 };
 
 makeRows(4, 4);
 
-// let gridItem = document.getElementsByClassName("grid-item");
 let gridItems = document.getElementsByClassName("grid-item");
 
 function changeColor() {
-  // document.getElementById(`sketch${i}`).style.backgroundColor = "magenta";
   gridItems[4].style.backgroundColor = "peachpuff";
   console.log("1");
 }
 
-// document.getElementsByClassName("grid-item").addEventListener("mouseover", changeColor); 
+function generateRandomColor() {
+  var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
 
 for (i=0; i < gridItems.length; i++) {
-  // gridItems[i].addEventListener("mouseenter",changeColor);
   gridItems[i].addEventListener("mouseenter", function(e) {
-    e.target.style.background = "papayawhip";
+    e.target.style.background = generateRandomColor();
   })
+}
+
+//make it so makeRows takes user input
+
+// make it so there's a reset button that wipes the grid the clean
+function resetGrid() {
+ // need to add event listener (might not need this
+ // when event happens, it should trigger the deleting the background color from every object that belongs to the class grid-item
+ for (i=0; i < gridItems.length; i++) {
+  let x = document.getElementById(i);
+  x.style.backgroundColor = null;
+  console.log('crap');
+ }
 }
 
 // include userInput box so the user can determine the size of the grid *work on this next I think it would be the easiest to get working
